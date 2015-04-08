a77e2ca01ebf079b0908868a3c7760a7f10d7852
Robert Bevilacqua <rbevilacqua11@gmail.com>
2015-04-08 17:34:27 -0430

    Init Project

---
+++ SocialNetworking/MainController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialNetworking
+//
+//  Created by Mohamed DIb on 4/8/15.
+//  Copyright (c) 2015 UpperSky. All rights reserved.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SocialNetworking/WebViewClass.swift
@@ -0,0 +1,18 @@
+//
+//  WebViewClass.swift
+//  SocialNetworking
+//
+//  Created by Mohamed DIb on 4/8/15.
+//  Copyright (c) 2015 UpperSky. All rights reserved.
+//
+
+import UIKit
+
+class WebViewClass: NSObject {
+
+    var url: NSURL = NSURL()
+    var accessToken: String = String()
+    var code: String = String()
+    var userSession: String = String()
+   
+}
